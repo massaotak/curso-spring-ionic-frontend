ca13238ffba6fe97542e4fd3148e057c1bf1b64c
Marcos Massao <massaotak@gmail.com>
2020-05-19 06:42:13 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:9090"
+    baseUrl: "http://localhost:9090",
+    bucketBaseUrl: "https://massao-spring-ionic.s3-sa-east-1.amazonaws.com"
 }
